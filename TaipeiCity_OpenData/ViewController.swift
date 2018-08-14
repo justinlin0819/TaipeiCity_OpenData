8ad036721a1a25b5781903ba1182a1c61fc5d231
Your Name <you@example.com>
2018-08-14 16:30:11 +0800

    Initial.

--- TaipeiCity_OpenData/ViewController.swift
+++ TaipeiCity_OpenData/ViewController.swift
@@ -13,8 +13,18 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let urlStr = "http://data.taipei/opendata/datalist/apiAccess?scope=resourceAquire&rid=e6831708-02b4-4ef8-98fa-4b4ce53459d9"
+        if let url = URL(string: urlStr) {
+            let task = URLSession.shared.dataTask(with: url) { (data, response , error) in
+                if let data = data, let dic = try?
+                    JSONSerialization.jsonObject(with: data) as? [String: Any], let results = dic?["results"] as? [[String: Any]] {
+                    for location in results {
+                        print(location["locationName"])
+                    }
+                }
+            }
+            task.resume()
+        }
     }
-
-
 }
 
