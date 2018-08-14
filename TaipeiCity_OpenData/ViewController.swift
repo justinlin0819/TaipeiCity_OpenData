8df9e2e24341f9238dc28ff110cd8fe0698f32e3
Your Name <you@example.com>
2018-08-14 15:50:52 +0800

    Initial Commit

---
+++ TaipeiCity_OpenData/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TaipeiCity_OpenData
+//
+//  Created by Justin Lin on 2018/8/14.
+//  Copyright © 2018年 Justin Lin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
